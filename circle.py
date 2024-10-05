addfd492b7438372abb280fc6c18eb7961b5586e
camperfail86 <mega.kochkin@inbox.ru>
2024-10-05 09:20:26 +0300

    added info circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,9 @@ import math
 
 
 def area(r):
-    return math.pi * r * r
-
+''' Takes the number r - the radius of the circle, returns the area of the circle '''
+return math.pi * r * r
 
 def perimeter(r):
-    return 2 * math.pi * r
-
+''' Takes the number r - the radius of the circle, returns the perimeter of the circle'''
+return 2 * math.pi * r
