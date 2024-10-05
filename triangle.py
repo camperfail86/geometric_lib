178d6f627d911fce2471969c1d28cacef4bd5fa1
camperfail86 <mega.kochkin@inbox.ru>
2024-10-05 09:25:26 +0300

    added info triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,15 @@
 def area(a, b, c):
     return (a + b + c) / 2
-
+''' Takes 2 numbers:
+a is the first side of the triangle,
+b is the second side of the triangle,
+c is the third side of the triangle.
+Returns the area of the triangle'''
 
 def perimeter(a, b, c):
     return a + b + c
+''' Takes 2 numbers:
+a is the first side of the triangle,
+b is the second side of the triangle,
+c is the third side of the triangle.
+Returns the perimeter of the triangle'''
